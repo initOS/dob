b1dd8a9cab135aa838baa4c9f3b65ea4b301c902
Ruben Ortlam <ruben.ortlam@initos.com>
2021-07-30 15:40:08 +0200

    Only set DOB_TAG when not running on CI

--- setup.sh
+++ setup.sh
@@ -21,10 +21,10 @@ UMASK=$(umask)
 DB_PASSWORD=$(cat /dev/urandom | tr -d -c "[:alnum:]" | head -c 25)
 
 ODOO_ADMIN_PASSWD=$(cat /dev/urandom | tr -d -c "[:alnum:]" | head -c 25)
-
-DOB_TAG=$(pwd | sha256sum | head -c 10)
 EOL
-elif [ -z $(grep "DOB_TAG=" ".env") ]; then
+fi
+
+if [ -z $(grep "DOB_TAG=" ".env") ] && [ -z "$CI_JOB_ID" ]; then
   echo -e "\033[0;32mAdding DOB_TAG to .env\033[0m"
   printf "\nDOB_TAG=$(pwd | sha256sum | head -c 10)" >> .env
 fi
